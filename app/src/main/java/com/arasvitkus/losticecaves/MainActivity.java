2464eddb5887cd65eedab95b1d48d6ac66d1cf35
arasmv <amvitkus@gmail.com>
2019-06-04 14:35:39 -0400

    Added initial story structure. Story index tracks the users choices and updates the text and buttons correctly. When reaching the end of the short story, rotate the phone to reset the app.

--- app/src/main/java/com/arasvitkus/losticecaves/MainActivity.java
+++ app/src/main/java/com/arasvitkus/losticecaves/MainActivity.java
@@ -2,12 +2,70 @@ package com.arasvitkus.losticecaves;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Member variables declared
+    private TextView mStoryTextView;
+    private Button mTopButton;
+    private Button mBottomButton;
+    private int mStoryIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup the three views from the layout to the member variables.
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mTopButton = findViewById(R.id.buttonTop);
+        mBottomButton = findViewById(R.id.buttonBottom);
+
+
+        //Top button onclicklistener
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mStoryIndex == 1) {
+                    mStoryTextView.setText(R.string.T2_Story);
+                    mTopButton.setText(R.string.T2_Ans1);
+                    mBottomButton.setText(R.string.T2_Ans2);
+                    mStoryIndex = 2;
+                } else if (mStoryIndex == 2) {
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mTopButton.setText(R.string.T3_Ans1);
+                    mBottomButton.setText(R.string.T3_Ans2);
+                    mStoryIndex = 3;
+                } else if (mStoryIndex == 3) {
+                    mStoryTextView.setText(R.string.T5_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        //Bottom button onclicklistener set up
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mStoryIndex == 1) {
+                    mStoryTextView.setText(R.string.T7_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+                } else if (mStoryIndex == 2) {
+                    mStoryTextView.setText(R.string.T4_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+                } else {
+                    mStoryTextView.setText(R.string.T6_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+
+                }
+            }
+        });
     }
 }
